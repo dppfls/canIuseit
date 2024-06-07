3866db5d70be6c5815703dd1ca2f6e35e1e055dd
jo0on <workjh77@gmail.com>
2024-06-07 15:31:48 +0900

    Update and rename backend/social-login-app/app.js to backend/app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const passport = require('passport');
 const session = require('express-session');
+const bodyParser = require('body-parser');
+const expirationRoutes = require('./routes/expiration');
+const { sequelize, initDb } = require('./models');
 require('dotenv').config();
 require('./config/passport-setup');
 
@@ -17,8 +20,12 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Body Parser 미들웨어 설정
+app.use(bodyParser.json());
+
 // 라우트 설정
 app.use('/auth', require('./routes/auth'));
+app.use('/api', expirationRoutes);
 
 app.get('/', (req, res) => {
   res.send('<h1>Home Page</h1><a href="/auth/google">Login with Google</a><br><a href="/auth/naver">Login with Naver</a>');
@@ -31,6 +38,18 @@ app.get('/profile', (req, res) => {
   res.send(`<h1>Profile Page</h1><p>${JSON.stringify(req.user)}</p>`);
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+    await initDb();
+
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+startServer();
