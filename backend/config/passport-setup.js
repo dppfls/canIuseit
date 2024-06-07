800d176c95d784f6a3c3680373ed6e81ca7ad847
jo0on <workjh77@gmail.com>
2024-06-07 15:35:47 +0900

    소셜 로그인 구현 초안 - 준희

--- backend/config/passport-setup.js
+++ backend/config/passport-setup.js
@@ -1,6 +1,8 @@
+// 변수 정리 해야함
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const NaverStrategy = require('passport-naver').Strategy;
+
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
