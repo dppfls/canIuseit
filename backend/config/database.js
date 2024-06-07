5033403af7915b6aad263e2fc51a749170b1980f
jo0on <workjh77@gmail.com>
2024-06-07 15:35:17 +0900

    소비기한 계산 구현 초안 - 준희

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-// 변경해야할 것 같음
+// 변경해야할 것 같음!
 const sequelize = new Sequelize('database', 'username', 'password', {
   host: 'localhost',  
   dialect: 'mysql',
