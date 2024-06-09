5a85a39196613c1ec48ea3ac3c934846285eebdb
jo0on <workjh77@gmail.com>
2024-06-10 00:23:51 +0900

    Update initDb.js

--- backend/models/initDb.js
+++ backend/models/initDb.js
@@ -4,16 +4,16 @@ const User = require('./User');
 const Product = require('./Product');
 
 const initDb = async () => {
-    await sequelize.sync({ force: true });
-    console.log('Database synchronized');
+  await sequelize.sync({ force: true });
+  console.log('Database synchronized');
 
-    // 카테고리별 소비기한 기준 데이터 초기화
-    await Category.bulkCreate([
-        { name: 'Dairy', defaultShelfLifeDays: 7 },
-        { name: 'Meat', defaultShelfLifeDays: 5 },
-        { name: 'Vegetables', defaultShelfLifeDays: 10 },
-        // 추가 카테고리 데이터를 여기 추가
-    ]);
+  // 카테고리별 소비기한 기준 데이터 초기화 // 이거 우리 카테고리 번호로 해야하는거잖아..!! -준희
+  await Category.bulkCreate([
+    { name: 'Dairy', shelfLifeDays: 7 },
+    { name: 'Meat', shelfLifeDays: 5 },
+    { name: 'Vegetables', shelfLifeDays: 10 },
+      
+  ]);
 };
 
 module.exports = { sequelize, initDb, Category, User, Product };
