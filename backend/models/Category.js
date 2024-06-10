b081c8c798633df4f341a5e203d41acfa6d1290a
jo0on <workjh77@gmail.com>
2024-06-10 16:37:59 +0900

    Update Category.js

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,17 +1,20 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
+// Category 모델 정의
 const Category = sequelize.define('Category', {
   name: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true,
+    unique: true, // 카테고리 이름은 고유해야 함
   },
   shelfLifeDays: {
     type: DataTypes.INTEGER,
-    allowNull: false,
+    allowNull: false, // 보관 기한은 필수 입력 항목
   },
+}, {
+  tableName: 'categories', // 테이블 이름 명시적 지정
+  timestamps: false, // 타임스탬프 비활성화
 });
 
 module.exports = Category;
-
