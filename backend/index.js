70f11ce4ccb77462653c0d080d403d436acb07d4
jo0on <workjh77@gmail.com>
2024-06-11 14:19:49 +0900

    be&fe

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
@@ -5,27 +6,25 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
 const FileStore = require('session-file-store')(session);
-const dotenv = require('dotenv');
-dotenv.config();
 const cors = require('cors');
 const flash = require('connect-flash');
 
 const authRoutes = require('./routes/auth');
 const expirationRoutes = require('./routes/expiration');
-const sequelize = require('./models/sequelize');
-const initDb = require('./models/initDb');
+const sequelize = require('./config/database');
+const { initDb } = require('./models/initDb');
 require('./config/passport-setup');
 
 const app = express();
 
 // CORS 설정
 app.use(cors({
-    origin: 'http://localhost:8080',
+    origin: 'http://localhost:8000',
 }));
 
 // 세션 설정
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || 'default_secret',
     resave: false,
     saveUninitialized: false,
     store: new FileStore()
@@ -50,12 +49,33 @@ app.use((req, res, next) => {
 app.use('/auth', authRoutes);
 app.use('/api', expirationRoutes);
 
+
 // 정적 파일 제공 설정
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..', 'frontend', 'resources')));
+
+// EJS 템플릿 엔진 설정
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '..', 'frontend', 'views'));
 
 // 기본 라우트 설정
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.render('index');
+});
+
+app.get('/look', (req, res) => {
+    res.render('look'); 
+});
+
+app.get('/calendar', (req, res) => {
+    res.render('calendar'); 
+});
+
+app.get('/login', (req, res) => {
+    res.render('login'); 
+});
+
+app.get('/label', (req, res) => {
+    res.render('label'); 
 });
 
 const PORT = process.env.PORT || 3000;
