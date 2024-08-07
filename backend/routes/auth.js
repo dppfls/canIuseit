1818a25250fd648c911e529ef6f167617c3048ab
Yeongeunn <Yeongeunn>
2024-08-07 21:31:29 +0900

    로그아웃 수정...

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -23,4 +23,18 @@ router.get('/naver/callback',
   authController.naverCallbackSuccess
 );
 
+// 로그아웃 라우트 추가
+router.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            console.error('Logout error:', err); // 로그아웃 에러 로그
+            return next(err);
+        }
+        req.session.destroy(() => {
+            console.log('Session destroyed'); // 세션 삭제 로그
+            res.redirect('/login'); // 세션을 삭제한 후 로그인 페이지로 리디렉션
+        });
+    });
+});
+
 module.exports = router;
